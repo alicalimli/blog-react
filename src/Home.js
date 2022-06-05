6351a13923abe98f7cba61b35db1a79499962992
alicalimli <alicalimli76@gmail.com>
2022-06-05 15:56:37 +0300

    added blog preview in the homepage

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,44 @@
 import React from "react";
+import { useState } from "react";
 
 const Home = () => {
+  const [blogs, setBlogs] = useState([
+    {
+      title: "Day 1 of Learning React",
+      body: "Here's what i've learned in my day 1",
+      author: "Ali Calimli",
+      id: 1,
+    },
+    {
+      title: "Day 1 of Learning React",
+      body: "Here's what i've learned in my day 1",
+      author: "Ali Calimli",
+      id: 1,
+    },
+    {
+      title: "Day 1 of Learning React",
+      body: "Here's what i've learned in my day 1",
+      author: "Ali Calimli",
+      id: 1,
+    },
+    {
+      title: "Day 1 of Learning React",
+      body: "Here's what i've learned in my day 1",
+      author: "Ali Calimli",
+      id: 1,
+    },
+  ]);
+
   return (
     <div className="home-page">
-      <h1>Home Page</h1>
+      <div className="blog-container">
+        {blogs.map((blogData) => (
+          <div className="blog-preview" key={blogData.key}>
+            <h2>{blogData.title}</h2>
+            <p>Author {blogData.author}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };

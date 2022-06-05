1882197f59910b469da63e47aaec047a82368fa1
alicalimli <alicalimli76@gmail.com>
2022-06-05 13:33:53 +0300

    added home page component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
   render() {
+    const name = "ali";
+    const lastname = "calimli";
     return (
       <div className="App">
         <div className="content">
-          <h1>Hello World</h1>
+          <h1>
+            Welcome, {name} {lastname}
+          </h1>
         </div>
       </div>
     );

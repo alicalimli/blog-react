5970be5b51ec616498c84c5741bc66fc43053c4e
alicalimli <alicalimli76@gmail.com>
2022-06-05 13:41:44 +0300

    bug fix

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import React from "react";
+import { createRoot } from "react-dom/cjs/react-dom.production.min";
+import { StrictMode } from "react/cjs/react.production.min";
+import App from "./App";
+import "./index.css";
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
 );

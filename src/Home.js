382fe1490e2b91a501537998bbfebbb4bcdb82e9
alicalimli <alicalimli76@gmail.com>
2022-06-05 18:23:22 +0300

    error handling

--- src/Home.js
+++ src/Home.js
@@ -4,30 +4,46 @@ import BlogList from "./BlogList";
 
 const Home = () => {
   const [blogs, setBlogs] = useState(null);
-  const [isPending, setIsPending] = useState(true)
+  const [isPending, setIsPending] = useState(true);
+  const [errorMessage, setErrorMessage] = useState(null);
 
   const dataFetch = async function(url){
-    const response = await fetch(url);
-    const results = await response.json();
+    try{
+      const response = await fetch(url);
 
-    return results;
+      if(!response.ok) throw new Error("Can't fetch resources.");
+
+      const results = await response.json();
+  
+      return results;
+    }
+    catch(error){
+      throw error;
+    }
   }
 
   useEffect(async () =>{
-    const blogDatas = await dataFetch('http://localhost:8000/blogs')
+    try{
+      setErrorMessage(null)
+      
+      const blogDatas = await dataFetch('http://localhost:8000/blogs');
 
-    setBlogs(await blogDatas);
+      setBlogs(await blogDatas);
 
-    blogDatas.then(setIsPending(false))
-
+      blogDatas.then(setIsPending(false));
+    }
+    catch(error){
+      console.log(error)
+      setErrorMessage(error.message);
+      setIsPending(false)
+    }
   }, [])
 
   return (
     <div className="home-page">
       <div className="blog-container">
-        {isPending && <h1 style={{
-          textAlign: 'center',
-        }}>Loading...</h1>}
+        {errorMessage && <h1 style={{textAlign: 'center'}}>{errorMessage}</h1>}
+        {isPending && <h1 style={{textAlign: 'center'}}>Loading...</h1>}
         {blogs && <BlogList blogs={blogs} title="All the Blogs"/> }
       </div>
     </div>

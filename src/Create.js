779c7c8034fc15ace9f6a0980aeb21dd3d7f0e56
alicalimli <alicalimli76@gmail.com>
2022-06-06 11:56:15 +0300

    added form in create blog component

--- src/Create.js
+++ src/Create.js
@@ -1,9 +1,36 @@
 import React from 'react';
+import { useState } from 'react';
 
 const Create = () => {
+
+  const [title,setTitle] = useState("")
+  const [body,setBody] = useState("")
+  const [author,setAuthor] = useState("")
+
   return ( 
-    <div>
-      <h1>Create a Blog</h1>
+    <div className='create'>
+      <h2>Create a Blog</h2>
+      <form>
+        <label>Blog title:</label>
+        <input 
+          type="text" 
+          required 
+          value={title}
+          onChange={(e)=> setTitle(e.target.value)}
+        />
+        <label>Blog body:</label>
+        <textarea
+          required
+          value={body}
+          onChange={(e)=> setBody(e.target.value)}
+        ></textarea>
+        <label>Blog author:</label>
+        <select value={author} onChange={(e)=> setAuthor(e.target.value) }>
+          <option value="mario">mario</option>
+          <option value="yoshi">yoshi</option>
+        </select>
+        <button>Add Blog</button>
+      </form>
     </div>
   );
 }
